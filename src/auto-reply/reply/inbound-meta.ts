sunnysylab/openclaw

Discord: resolve trusted principals via identity links

src/auto-reply/reply/inbound-meta.ts
@@ -52,6 +52,7 @@ export function buildInboundMetaSystemPrompt(ctx: TemplateContext): string {
     schema: "openclaw.inbound_meta.v1",
     chat_id: safeTrim(ctx.OriginatingTo),
     account_id: safeTrim(ctx.AccountId),
+    sender_principal: safeTrim(ctx.TrustedSenderPrincipal),
     channel: channelValue,
     provider: safeTrim(ctx.Provider),
     surface: safeTrim(ctx.Surface),

auraecosystem/Openclaw-builder

src/auto-reply/reply/inbound-meta.ts
@@ -411,6 +411,7 @@ export function buildInboundMetaSystemPrompt(
   const payload = {
     schema: "openclaw.inbound_meta.v2",
     account_id: normalizePromptMetadataString(ctx.AccountId),
+    sender_principal: normalizePromptMetadataString(ctx.TrustedSenderPrincipal),
     channel: channelValue,
     provider: normalizePromptMetadataString(ctx.Provider),
     surface: normalizePromptMetadataString(ctx.Surface),