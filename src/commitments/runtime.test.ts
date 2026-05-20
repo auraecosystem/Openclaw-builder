MoerAI/openclaw

fix(commitments): stop hardcoding fastMode in extraction (#78451)
MiniMax Coding Plan operators whose plan owns MiniMax-M2.7 but not the -highspeed variant hit HTTP 500 'your current token plan not support model, MiniMax-M2.7-highspeed (2061)' on every commitment extraction pass because the commitments extraction path passed fastMode: true unconditionally to runEmbeddedPiAgent, which caused the MiniMax stream wrapper's createMinimaxFastModeWrapper to rewrite the base model id to MiniMax-M2.7-highspeed. Drop the hardcoded literal so commitment extraction inherits the agent/config-resolved fast-mode state (default false) just like every other agent run path. Adds runtime.test.ts assertion that fastMode is no longer in the runEmbeddedPiAgent payload to lock in the fix.

src/commitments/runtime.test.ts
@@ -202,6 +202,12 @@ describe("commitment extraction runtime", () => {
         disableTools: true,
       }),
     );
+    // Regression for #78451: commitment extraction must NOT hardcode fastMode,
+    // so MiniMax (and similar) stream wrappers do not flip the configured
+    // base model id to a -highspeed variant the operator's plan does not own.
+    const callArgs = runEmbeddedPiAgentMock.mock.calls[0]?.[0];
+    expect(callArgs).toBeDefined();
+    expect(callArgs).not.toHaveProperty("fastMode");
   });
 
   it("backs off hidden extraction after terminal model or auth failures", async () => {

auraecosystem/Openclaw-builder

src/commitments/runtime.test.ts
@@ -224,6 +224,10 @@ describe("commitment extraction runtime", () => {
     expect(request.provider).toBe("openai-codex");
     expect(request.model).toBe("gpt-5.5");
     expect(request.disableTools).toBe(true);
+    // Regression for #78451: commitment extraction must NOT hardcode fastMode,
+    // so MiniMax (and similar) stream wrappers do not flip the configured
+    // base model id to a -highspeed variant the operator's plan does not own.
+    expect(request).not.toHaveProperty("fastMode");
   });
 
   it("backs off hidden extraction after terminal model or auth failures", async () => {