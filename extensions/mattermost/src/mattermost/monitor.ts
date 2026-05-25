14c80b131292901f2ac7062e55b0217846cf783c
Peter Steinberger <steipete@gmail.com>
2026-05-25 22:09:04 +0100

    refactor: replace channel turn runtime API

--- extensions/mattermost/src/mattermost/monitor.ts
+++ extensions/mattermost/src/mattermost/monitor.ts
@@ -1790,7 +1790,7 @@ export async function monitorMattermostProvider(opts: MonitorMattermostOpts = {}
 
         let dispatchSettledBeforeStart = false;
         try {
-          await core.channel.turn.run({
+          await core.channel.inbound.run({
             channel: "mattermost",
             accountId: route.accountId,
             raw: post,
