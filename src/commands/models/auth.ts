openclaw/openclaw

fix(auth): short-circuit token prompt when --token flag is supplied

src/commands/models/auth.ts
@@ -372,6 +372,7 @@ export async function modelsAuthPasteTokenCommand(
     provider?: string;
     profileId?: string;
     expiresIn?: string;
+    token?: string;
   },
   runtime: RuntimeEnv,
 ) {
@@ -384,19 +385,22 @@ export async function modelsAuthPasteTokenCommand(
   const profileId =
     normalizeOptionalString(opts.profileId) || resolveDefaultTokenProfileId(provider);
 
-  const tokenInput = await text({
-    message: `Paste token for ${provider}`,
-    validate: (value) => {
-      const trimmed = value?.trim();
-      if (!trimmed) {
-        return "Required";
-      }
-      if (provider === "anthropic") {
-        return validateAnthropicSetupToken(trimmed.replaceAll(/\s+/g, ""));
-      }
-      return undefined;
-    },
-  });
+  const rawToken = normalizeOptionalString(opts.token);
+  const tokenInput =
+    rawToken ??
+    (await text({
+      message: `Paste token for ${provider}`,
+      validate: (value) => {
+        const trimmed = value?.trim();
+        if (!trimmed) {
+          return "Required";
+        }
+        if (provider === "anthropic") {
+          return validateAnthropicSetupToken(trimmed.replaceAll(/\s+/g, ""));
+        }
+        return undefined;
+      },
+    }));
   const token =
     provider === "anthropic"
       ? String(tokenInput ?? "")

auraecosystem/Openclaw-builder

src/commands/models/auth.ts
@@ -573,6 +573,7 @@ export async function modelsAuthPasteTokenCommand(
     profileId?: string;
     expiresIn?: string;
     agent?: string;
+    token?: string;
   },
   runtime: RuntimeEnv,
 ) {
@@ -587,23 +588,31 @@ export async function modelsAuthPasteTokenCommand(
   const profileId =
     normalizeOptionalString(opts.profileId) || resolveDefaultTokenProfileId(provider);
 
-  const tokenInput = await readPastedSecret({
-    message: `Paste token for ${provider}`,
-    masked: false,
-    validate: (value) => {
-      const trimmed = value?.trim();
-      if (!trimmed) {
-        return "Required";
-      }
-      if (provider === "anthropic") {
-        return validateAnthropicSetupToken(trimmed.replaceAll(/\s+/g, ""));
-      }
-      if (isOpenAICodexProvider(provider) && looksLikeOpenAIApiKey(trimmed)) {
-        return `That looks like an OpenAI API key. Use ${formatCliCommand("openclaw models auth paste-api-key --provider openai-codex")} for API-key auth.`;
-      }
-      return undefined;
-    },
-  });
+  const validateTokenInput = (value: string | undefined): string | undefined => {
+    const trimmed = value?.trim();
+    if (!trimmed) {
+      return "Required";
+    }
+    if (provider === "anthropic") {
+      return validateAnthropicSetupToken(trimmed.replaceAll(/\s+/g, ""));
+    }
+    if (isOpenAICodexProvider(provider) && looksLikeOpenAIApiKey(trimmed)) {
+      return `That looks like an OpenAI API key. Use ${formatCliCommand("openclaw models auth paste-api-key --provider openai-codex")} for API-key auth.`;
+    }
+    return undefined;
+  };
+  const rawToken = normalizeOptionalString(opts.token);
+  const rawTokenValidation = rawToken ? validateTokenInput(rawToken) : undefined;
+  if (rawTokenValidation) {
+    throw new Error(rawTokenValidation);
+  }
+  const tokenInput =
+    rawToken ??
+    (await readPastedSecret({
+      message: `Paste token for ${provider}`,
+      masked: false,
+      validate: validateTokenInput,
+    }));
   const token =
     provider === "anthropic"
       ? tokenInput.replaceAll(/\s+/g, "").trim()