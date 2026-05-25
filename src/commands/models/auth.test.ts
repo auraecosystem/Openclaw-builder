Anas329796/Anastasios

docs(auth): document paste-token token flag

src/commands/models/auth.test.ts
@@ -21,6 +21,7 @@ const mocks = vi.hoisted(() => ({
   logConfigUpdated: vi.fn(),
   openUrl: vi.fn(),
   isRemoteEnvironment: vi.fn(() => false),
+  validateAnthropicSetupToken: vi.fn<() => string | undefined>(() => undefined),
   loadAuthProfileStoreForRuntime: vi.fn(),
   listProfilesForProvider: vi.fn(),
   promoteAuthProfileInOrder: vi.fn(),
@@ -127,7 +128,7 @@ vi.mock("../../plugins/provider-oauth-flow.js", () => ({
 }));
 
 vi.mock("../auth-token.js", () => ({
-  validateAnthropicSetupToken: vi.fn(() => undefined),
+  validateAnthropicSetupToken: mocks.validateAnthropicSetupToken,
 }));
 
 vi.mock("../../plugins/provider-auth-choice-helpers.js", () => {
@@ -279,6 +280,8 @@ describe("modelsAuthLoginCommand", () => {
     );
     mocks.clackSelect.mockReset();
     mocks.clackText.mockReset();
+    mocks.validateAnthropicSetupToken.mockReset();
+    mocks.validateAnthropicSetupToken.mockReturnValue(undefined);
     mocks.upsertAuthProfile.mockReset();
     mocks.promoteAuthProfileInOrder.mockReset();
 
@@ -806,6 +809,51 @@ describe("modelsAuthLoginCommand", () => {
       },
       agentDir: "/tmp/openclaw/agents/coder",
     });
+  });
+
+  it("writes --token values without prompting", async () => {
+    const runtime = createRuntime();
+
+    await modelsAuthPasteTokenCommand({ provider: "openai", token: "openai-token" }, runtime);
+
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfile).toHaveBeenCalledWith({
+      profileId: "openai:manual",
+      credential: {
+        type: "token",
+        provider: "openai",
+        token: "openai-token",
+      },
+      agentDir: "/tmp/openclaw/agents/main",
+    });
+  });
+
+  it("rejects empty --token values before prompting", async () => {
+    const runtime = createRuntime();
+
+    await expect(
+      modelsAuthPasteTokenCommand({ provider: "openai", token: "   " }, runtime),
+    ).rejects.toThrow("--token value must not be empty");
+
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfile).not.toHaveBeenCalled();
+    expect(mocks.updateConfig).not.toHaveBeenCalled();
+  });
+
+  it("validates Anthropic --token values before persisting", async () => {
+    const runtime = createRuntime();
+    mocks.validateAnthropicSetupToken.mockReturnValue(
+      "Anthropic setup-token must start with sk-ant-oat01-",
+    );
+
+    await expect(
+      modelsAuthPasteTokenCommand({ provider: "anthropic", token: "not-anthropic" }, runtime),
+    ).rejects.toThrow("Anthropic setup-token must start with sk-ant-oat01-");
+
+    expect(mocks.validateAnthropicSetupToken).toHaveBeenCalledWith("not-anthropic");
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfile).not.toHaveBeenCalled();
+    expect(mocks.updateConfig).not.toHaveBeenCalled();
   });
 
   it("rejects an unknown agent before prompting for pasted tokens", async () => {

auraecosystem/Openclaw-builder

src/commands/models/auth.test.ts
@@ -55,6 +55,7 @@ const mocks = vi.hoisted(() => ({
   logConfigUpdated: vi.fn(),
   openUrl: vi.fn(),
   isRemoteEnvironment: vi.fn(() => false),
+  validateAnthropicSetupToken: vi.fn<() => string | undefined>(() => undefined),
   loadAuthProfileStoreForRuntime: vi.fn(),
   listProfilesForProvider: vi.fn(),
   promoteAuthProfileInOrder: vi.fn(),
@@ -170,7 +171,7 @@ vi.mock("../../plugins/provider-oauth-flow.js", () => ({
 }));
 
 vi.mock("../auth-token.js", () => ({
-  validateAnthropicSetupToken: vi.fn(() => undefined),
+  validateAnthropicSetupToken: mocks.validateAnthropicSetupToken,
 }));
 
 vi.mock("../../plugins/provider-auth-choice-helpers.js", async (importOriginal) => {
@@ -356,6 +357,8 @@ describe("modelsAuthLoginCommand", () => {
     mocks.clackPassword.mockReset();
     mocks.clackSelect.mockReset();
     mocks.clackText.mockReset();
+    mocks.validateAnthropicSetupToken.mockReset();
+    mocks.validateAnthropicSetupToken.mockReturnValue(undefined);
     mocks.upsertAuthProfileWithLock.mockReset();
     mocks.upsertAuthProfileWithLock.mockResolvedValue({ version: 1, profiles: {} });
     mocks.promoteAuthProfileInOrder.mockReset();
@@ -1211,6 +1214,51 @@ describe("modelsAuthLoginCommand", () => {
       },
       agentDir: "/tmp/openclaw/agents/coder",
     });
+  });
+
+  it("writes --token values without prompting", async () => {
+    const runtime = createRuntime();
+
+    await modelsAuthPasteTokenCommand({ provider: "openai", token: "openai-token" }, runtime);
+
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfileWithLock).toHaveBeenCalledWith({
+      profileId: "openai:manual",
+      credential: {
+        type: "token",
+        provider: "openai",
+        token: "openai-token",
+      },
+      agentDir: "/tmp/openclaw/agents/main",
+    });
+  });
+
+  it("rejects empty --token values before prompting", async () => {
+    const runtime = createRuntime();
+
+    await expect(
+      modelsAuthPasteTokenCommand({ provider: "openai", token: "   " }, runtime),
+    ).rejects.toThrow("--token value must not be empty");
+
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfileWithLock).not.toHaveBeenCalled();
+    expect(mocks.updateConfig).not.toHaveBeenCalled();
+  });
+
+  it("validates Anthropic --token values before persisting", async () => {
+    const runtime = createRuntime();
+    mocks.validateAnthropicSetupToken.mockReturnValue(
+      "Anthropic setup-token must start with sk-ant-oat01-",
+    );
+
+    await expect(
+      modelsAuthPasteTokenCommand({ provider: "anthropic", token: "not-anthropic" }, runtime),
+    ).rejects.toThrow("Anthropic setup-token must start with sk-ant-oat01-");
+
+    expect(mocks.validateAnthropicSetupToken).toHaveBeenCalledWith("not-anthropic");
+    expect(mocks.clackText).not.toHaveBeenCalled();
+    expect(mocks.upsertAuthProfileWithLock).not.toHaveBeenCalled();
+    expect(mocks.updateConfig).not.toHaveBeenCalled();
   });
 
   it("rejects OpenAI API keys pasted as OpenAI Codex token material", async () => {